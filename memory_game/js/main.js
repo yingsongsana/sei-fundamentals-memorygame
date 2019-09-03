a73e84759dc2968665935df8703fa368a60a5967
Ying Songsana <ying@Yings-MacBook-Pro.local>
2019-09-03 10:39:43 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,23 @@
-console.log("Up and running!");
-
 cards = ["queen", "queen", "king", "king"];
 cardsInPlay = [];
 
-const cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-const cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2){
+function checkForMatch(){
 	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
+		console.log("You found a match!");
 	}
 	else{
-		alert("Sorry, try again.");
+		console.log("Sorry, try again.");
 	}
 }
+
+function flipCard(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+	}
+}
+
+flipCard(0);
+flipCard(2);
+

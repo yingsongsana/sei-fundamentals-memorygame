4a44582189c48374aac6e299db8919a6dcf56272
Ying Songsana <ying@Yings-MacBook-Pro.local>
2019-09-04 11:58:16 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -31,16 +31,27 @@ function checkForMatch(){
 	}
 }
 
-function flipCard(cardId){
+function flipCard(){
+	var cardId = this.getAttribute("data-id");
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute("src", cards[cardId].cardImage);
 	if (cardsInPlay.length === 2){
 		checkForMatch();
 	}
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard(){
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+};
+
+createBoard();
 
